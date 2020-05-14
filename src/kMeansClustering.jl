f0370d832e38a7e3d39953aebad2ca3cb4def882
PHamacher <64922101+PHamacher@users.noreply.github.com>
2020-05-14 11:36:11 -0300

    Update kMeansClustering.jl

--- src/kMeansClustering.jl
+++ src/kMeansClustering.jl
@@ -1,40 +1,128 @@
-function f(n)
-#k=3
-l=[]
-for m in 1:n
-    p=rand(3)
-    push!(l,p)
-end
-centroides=rand(l,3)
-copia=[]
-while centroides!=copia
-    c1,c2,c3=[],[],[]
-    for p in l
-        d1=(p[1]-centroides[1][1])^2+(p[2]-centroides[1][2])^2+(p[3]-centroides[1][3])^2
-        d2=(p[1]-centroides[2][1])^2+(p[2]-centroides[2][2])^2+(p[3]-centroides[2][3])^2
-        d3=(p[1]-centroides[3][1])^2+(p[2]-centroides[3][2])^2+(p[3]-centroides[3][3])^2
-        menor=min(d1,d2,d3)
-        if menor==d1
-            push!(c1,p)
-        elseif menor==d2
-            push!(c2,p)
-        elseif menor==d3
-            push!(c3,p)
-        end
+function SquaredEuclideanDistance(v,w)
+    dist=0
+    for d in 1:length(v)
+        dist+=(v[d]-w[d])^2
     end
-    copia=centroides[1:3]
-    clusters=[c1,c2,c3]
+    return dist
+end
+
+function agrupa(matriz)
+    l=[]
+    dim=size(matriz)[1]
+    npontos=size(matriz)[2]
+    for p in 1:npontos
+        v=[]
+        for d in 1:dim
+            push!(v,matriz[dim*(p-1)+d])
+        end
+        push!(l,v)
+    end
+    return (l,dim)
+end
+
+function ComparaDist(p,antigoctr,novoctr)
+    antigadist=SquaredEuclideanDistance(p,antigoctr)
+    novadist=SquaredEuclideanDistance(p,novoctr)
+    if novadist<=antigadist
+        centroide=novoctr
+    else
+        centroide=antigoctr
+    end
+    return centroide
+end
+
+function dic(l,centroides)
+    dicCentroides=Dict()
+    for ctr in centroides
+        dicCentroides[ctr]=[]
+    end
+    for p in l
+        centroide=centroides[1]
+        for ctr in centroides
+            centroide=ComparaDist(p,centroide,ctr)
+        end
+        c=dicCentroides[centroide]
+        push!(c,p)
+        dicCentroides[centroide]=c
+    end
+    return dicCentroides
+end
+
+function reorganiza(clusters,dim)
     centroides=[]
     for cluster in clusters
-        x,y,z=0,0,0
+        s=zeros(dim)
         for p in cluster
-            x+=p[1]
-            y+=p[2]
-            z+=p[3]
+            for n in 1:dim
+                s[n]+=p[n]
+            end
         end
-        push!(centroides,[x/length(cluster),y/length(cluster),z/length(cluster)])
+        medias=[]
+        for d in s
+            push!(medias,d/length(cluster))
+        end
+        push!(centroides,medias)
     end
+    return centroides
 end
-return centroides
+
+function cost(dicCentroides)
+    custo=0
+    for (centr,clus) in dicCentroides
+        for p in clus
+            custo+=SquaredEuclideanDistance(p,centr)
+        end
+    end
+    return custo
 end
-println(f(10000))
+
+function atribui(clusters,l)
+    dicAssign=Dict()
+    for n in 1:length(clusters)
+        for el in clusters[n]
+            dicAssign[el]=n
+        end
+    end
+    lAssign=[]
+    for p in l
+        push!(lAssign,dicAssign[p])
+    end
+    return lAssign
+end
+
+function AdequaFormato(centroides)
+    novo=hcat(centroides[1],centroides[2])
+    for p in centroides[3:length(centroides)]
+        novo=hcat(novo,p)
+    end
+    return novo
+end
+
+function BuildClusters(dicCentroides)
+    clusters=[]
+    for (centroide,cluster) in dicCentroides
+        push!(clusters,cluster)
+    end
+    return clusters
+end
+
+function mykmeansclustering(matriz,k)
+    l,dim=agrupa(matriz)
+    centroides=[]
+    while length(unique(centroides))<k
+        centroides=rand(l,k) # Forgy Method
+    end
+    copia=[]
+    while centroides!=copia
+        dicCentroides=dic(l,centroides)
+        copia=centroides[1:k]
+        clusters=BuildClusters(dicCentroides)
+        centroides=reorganiza(clusters,dim)
+    end
+    dicCentroides=dic(l,centroides)
+    clusters=BuildClusters(dicCentroides)
+    custo=cost(dicCentroides)
+    assign=atribui(clusters,l)
+    centroides=AdequaFormato(centroides)
+    return centroides,custo,assign
+end
